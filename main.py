cca8068361f45bc535a721b01382f5ee50e92033
Baruch Youssin <SecretName@secret.address>
2022-01-09 11:45:55 +0200

    minor edits

--- main.py
+++ main.py
@@ -19,22 +19,22 @@ train = data[:train_length]
 
 epochs = 10000  # no batches: each epoch is one iteration
 lr_schedule = [1e-3] + 4 * [1e-3] + (epochs - 5) * [1e-2]
-# Learning usually stabilizes after a few hundred iterations, with no signs of overfit; for these particular seeds,
+# Learning usually stabilizes after a few hundred iterations, with no signs of overfit;
+# for these particular seed values,
 # it keeps improving slowly but significantly over the first few thousands.
 model = lib.Arima_0_1_1()
 optim = torch.optim.SGD(model.parameters(), lr=lr_schedule[0])
-previous_loss = None
 for n_iteration, lr in enumerate(lr_schedule):
     optim.param_groups[0]["lr"] = lr
     loss = lib.loss(model, train)
-    previous_loss = loss.item()
     optim.zero_grad()
     loss.backward()
     optim.step()
     print(f"Iter: {n_iteration}, Loss: {loss:.5f}, c={model.arma_const.item():.5f}, theta={model.ma_coeff().item():.5f}, "
           f"sigma:{model.std_innovation.item():.5f}")
 
-test = data[(train_length - 1):]
+test = data[(train_length - 1):]  # We include the last element of train,
+# see Probability of observing a time series in the doc
 loss_test = lib.loss(model, test)
 prob_density = (2 * math.pi) ** (-(data_length - train_length + 1) / 2) * math.exp(-loss_test / 2)
 prob_estimate = lib.prob_estimate(model, test)
